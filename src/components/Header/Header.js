601bddfef43328466775297ef743e9a65efea345
Vijay Menon <vijaymenonx@gmail.com>
2021-03-29 15:58:35 -0700

    Update search

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,15 +12,16 @@ const Header = ({text, darkMode, setDarkMode}) => (
                     <Link to="/"><img className={`logo`} src={`${darkMode ? `https://cdnwest.radnet.com/brand/logo-web-white.svg`: `https://cdnwest.radnet.com/brand/logo-radnet.svg`}`} alt={text} /></Link>
                     <p style={{fontSize:`0.4rem`}} className={`text-right pt-0 mt-0 logo-secondary`}>Videos</p>
                 </Col>
-                <Col sm="7" className="pl-5 pt-4">
+                <Col sm="8">
+                    <Search />
+                </Col>
+                <Col sm="2" className="pl-5 pt-4">
                     <ul className="list-inline">
                         <li className="list-inline-item"><Link to="/category">Browse</Link></li>
                         <li className="list-inline-item"><Link to="/about">About</Link></li>
                     </ul>
                 </Col>
-                <Col sm="3">
-                    <Search />
-                </Col>
+                
                 <Col sm="1" className="mt-3 text-right">
                 <span className="toggle" onClick={()=>setDarkMode(!darkMode)}>
                   <span className={`switch ${darkMode ? 'on' : '' }`}></span>

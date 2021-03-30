c1627cb79417be9a1392e811af124ccba0cf4053
Vijay Menon <vijaymenonx@gmail.com>
2021-03-29 19:39:01 -0700

    Category pages

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -5,34 +5,38 @@ import {replaceSpaces, tryFn} from "../../lib/util"
 import {setFilter} from '../../actions'
 import {Container, Card, Row, Col} from 'react-bootstrap'
 import Video from '../Video/Video'
+import { filter } from 'lodash-es'
+let token = process.env.REACT_APP_VIMEO_TOKEN
 
 export default function Category({filterData}){
 
-    //Refactor
-    let dispatch = useDispatch()
+    //get category (aka filter) from url
     let {categoryId} = useParams()
+    //adapt for the name property on the category data
     let categoryName = categoryId.replace(/-/g, " ")
     let data = filterData.filter(obj => obj.name === categoryName)[0]
+
+    //see if there's a uri (from vimeo) to get all the videos associated w/this filter
     let videoFilter = tryFn(()=>data.metadata.connections.videos.uri)
     let description = tryFn(()=>data.description)
-    let [loading, setLoading] = useState(false)
-    let videos = []
+
+    let [videos, setVideos] = useState([])
+    let uri = `https://api.vimeo.com/me${videoFilter}`
+
 
     useEffect(()=>{
-        //something to look into - why so many re-renders
-        //forcing time so the filter can get the right set of videos
-        setTimeout(()=>{
-            dispatch(setFilter(videoFilter))
-        }, 500)
-    }, [dispatch, videoFilter])
-    videos = useSelector((state)=>state.allVideos, shallowEqual)
-    let currentFilter = useSelector((state)=>state.currentFilter, shallowEqual)
-    //get videos associated with filter
-
-    let history = useHistory()
-
-    console.log(currentFilter, videoFilter);
-    let out = (currentFilter === videoFilter.replace("/albums/",'').replace('/videos', '')) ? (
+        fetch(uri, { 
+            headers: {
+            'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(data => data.json())
+        .then(res => {
+            setVideos(res.data)
+        })  
+    }, [uri])
+    
+    let out = videos.length > 0 ? (
         <div className="category">
             <div className="text-center">
             <h2 className="mt-5 pt-5">{categoryName}</h2>

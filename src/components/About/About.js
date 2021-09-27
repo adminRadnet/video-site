41a143b5abc45ec3d6f1f074425e2f1df2f54f1c
rodozlatan <rodolfoprodriguez@gmail.com>
2021-09-27 14:28:23 -0600

    -Added about us page
    Added preliminar layout for the about us page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,10 +3,138 @@ import Seo from '../../components/Seo'
 
 const About = () => {
     return (
-        <div className="max-width-lg">
-        <Seo title={`About`} />
-        <h2>About</h2>
-        </div>
+        <section id="about">
+            <div className="max-width-lg">
+                <Seo title={`About`} />
+                <div class="row">
+                    <div class="col mb-5 mt-5 header">
+                        <h1>Who  We Are</h1>
+                        <p>RadNet drives innovation in imaging. Forged from 35 years of experience, our portfolio aligns the broadest range of resources and expertise in the industry. We deliver high quality, cost-effective solutions that take all stakeholders - patients, providers, payors and regulators - into the future of healthcare.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-4">
+                        <div class="card mb-5">
+                            <h5>Growth</h5>
+                            <div class="card-body text-center">
+                                <h1 class="card-title">4x</h1>
+                                <blockquote class="blockquote mb-0">
+                                <p>Increase in Size Since 2006</p>
+                                </blockquote>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                        <div class="card mb-5">
+                            <h5>Size</h5>
+                            <div class="card-body text-center">
+                                <h1 class="card-title">350+</h1>
+                                <blockquote class="blockquote mb-0">
+                                <p>Centers Across the United States</p>
+                                </blockquote>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                        <div class="card mb-5">
+                            <h5>Quality</h5>
+                            <div class="card-body text-center">
+                                <h1 class="card-title">92%</h1>
+                                <blockquote class="blockquote mb-0">
+                                <p>2019 Patient Satisfaction Rating</p>
+                                </blockquote>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-4">
+                        <div class="card mb-5">
+                            <h5>Experience</h5>
+                            <div class="card-body text-center">
+                                <h1 class="card-title">35+</h1>
+                                <blockquote class="blockquote mb-0">
+                                <p>Years Leading the Imaging Industry</p>
+                                </blockquote>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                        <div class="card mb-5">
+                            <h5>Rank</h5>
+                            <div class="card-body text-center">
+                                <h1 class="card-title">#1</h1>
+                                <blockquote class="blockquote mb-0">
+                                <p>
+                                    Largest Outpatient Imaging Company in the US
+                                    <br />
+                                    <small>* RadiologyBusiness.com</small>
+                                </p>
+                                </blockquote>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                        <div class="card mb-5">
+                            <h5>Volume</h5>
+                            <div class="card-body text-center">
+                                <span class="w-50 d-inline-block">
+                                    <h1 class="card-title">8mil+</h1>
+                                    <blockquote class="blockquote mb-0">
+                                    <p>
+                                        Annual<br />Exams
+                                    </p>
+                                    </blockquote>
+                                </span>
+                                <span class="w-50 d-inline-block">
+                                    <h1 class="card-title">1.5mil+</h1>
+                                    <blockquote class="blockquote mb-0">
+                                    <p>
+                                        Annual <br /> Mammograms
+                                    </p>
+                                    </blockquote>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-4">
+                        <div class="card mb-5">
+                            <h5>Employees</h5>
+                            <div class="card-body text-center">
+                                <h1 class="card-title">8600+</h1>
+                                <blockquote class="blockquote mb-0">
+                                <p>Employees in 7 States</p>
+                                </blockquote>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                        <div class="card mb-5">
+                            <h5>Physicians</h5>
+                            <div class="card-body text-center">
+                                <h1 class="card-title">750x</h1>
+                                <blockquote class="blockquote mb-0">
+                                <p>Radiologists with Subspecialties</p>
+                                </blockquote>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                        <div class="card mb-5">
+                            <h5>Joint Ventures</h5>
+                            <div class="card-body text-center">
+                                <h1 class="card-title">19</h1>
+                                <blockquote class="blockquote mb-0">
+                                <p>Health systems use RadNet to achieve their business objectives</p>
+                                </blockquote>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
